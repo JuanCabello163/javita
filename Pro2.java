536d08aba8bfca5d385b3ee35f1928ebae6c0d1c
JuanCabello163 <cabellojuan717@gmail.com>
2023-08-17 11:38:13 -0500

    agregue la operacion suma

--- Pro2.java
+++ Pro2.java
@@ -17,7 +17,17 @@ public class Pro2 {
         //SUMAAAAAAA
         Scanner scanner = new Scanner(System.in);
         System.out.println("La Suma");
-
+        System.out.print("Ingrese el primer numero: ");
+        
+        int numero1 = scanner.nextInt();
+        
+        System.out.print("INGRESE EL SEGUNDO NUMERO: ");
+        
+        int numero2 = scanner.nextInt();
+        
+        int suma = numero1 + numero2;
+        System.out.println("La suma de " + numero1 + " + " + numero2 + " es: " + suma);
+        
     
     }
 }
