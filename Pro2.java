2dc83e59548b36072b764379d9da2a1bc472edab
JuanCabello163 <cabellojuan717@gmail.com>
2023-08-17 11:50:25 -0500

    agremamos la division a nuestra operacion

--- Pro2.java
+++ Pro2.java
@@ -44,5 +44,18 @@ public class Pro2 {
         int resta = numero3 - numero4;
         System.out.println("La resta de " + numero3 + " - " + numero4 + " es: " + resta);
         
+        System.out.println();
+        //DIVISIOOON
+        System.out.println("La Division");
+        
+        System.out.print("INGRESE EL PRIMER NUMERO: ");
+        int numero5 = scanner.nextInt();
+        
+        System.out.print("INGRESE EL SEGUNDO NUMERO: ");
+        int numero6 = scanner.nextInt();
+        
+        int division = numero5 / numero6;
+        System.out.println("La resta de " + numero5 + " / " + numero6 + " es: " + division);
+        
     }
 }
